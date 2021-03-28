587787835bd9d2f8a6f55e996c5f517c218833d6
jonkarrer <karrerjon@gmail.com>
2021-03-28 11:51:15 -0500

    made image overlays

--- comps/DestinImages.tsx
+++ comps/DestinImages.tsx
@@ -4,50 +4,38 @@ const DestinImages = () => (
   <>
     <div className="destin-container">
       <div className="left-group">
-        <Image
-          src="/Destinations/colorado_backpack.jpg"
-          alt="Picture of a colorado mountain"
-          height={300}
-          width={300}
-          className="colorado"
-        />
-        <Image
-          src="/Destinations/faye_canyon.JPG"
-          alt="Picture of faye canyon"
-          height={240}
-          width={320}
-          className="faye"
-        />
-        <Image
-          src="/Destinations/ozarks.jpg"
-          alt="Ozark mountains"
-          height={300}
-          width={230}
-          className="ozarks"
-        />
-        <Image
-          src="/Destinations/cathedral_rock.jpg"
-          alt="Red Rock Arizona pick"
-          height={300}
-          width={400}
-          className="cathedralRock"
-        />
+        <div className="colorado">
+          <div className="overlay">
+            <p>Colorado</p>
+          </div>
+        </div>
+        <div className="faye">
+          <div className="overlay">
+            <p>Faye Canyon</p>
+          </div>
+        </div>
+        <div className="ozarks">
+          <div className="overlay">
+            <p>Ozarks</p>
+          </div>
+        </div>
+        <div className="sedona">
+          <div className="overlay">
+            <p>Sedona</p>
+          </div>
+        </div>
       </div>
-      <Image
-        src="/Destinations/mt_ida.jpg"
-        alt="Picture of a camp on mt. ida Arkansas"
-        height={500}
-        width={500}
-        className="mtIda"
-      />
+      <div className="mt-ida">
+        <div className="overlay">
+          <p>Mt. Ida</p>
+        </div>
+      </div>
       <div className="right-group">
-        <Image
-          src="/Destinations/tennessee.jpg"
-          alt="Picture of a smokey mountain"
-          height={370}
-          width={370}
-          className="leconte"
-        />
+        <div className="leconte">
+          <div className="overlay">
+            <p>Tennessee</p>
+          </div>
+        </div>
         <Image
           src="/Destinations/kansas.jpg"
           alt="Picture of a camp in kansas"
@@ -71,11 +59,104 @@ const DestinImages = () => (
         display: grid;
         grid-template-columns: auto auto;
         grid-template-columns: auto auto;
-        height: 500px;
+        height: 400px;
+        width: auto;
       }
       .right-group {
         display: grid;
         height: 600px;
+      }
+      p {
+        font-size: 3em;
+        color: white;
+      }
+      .overlay {
+        display: none;
+        align-items: center;
+        justify-content: center;
+      }
+      .colorado {
+        background-image: url("/Destinations/colorado_backpack.jpg");
+        background-position: center;
+        background-size: cover;
+        height: 200px;
+        width: 270px;
+      }
+      .colorado:hover > .overlay {
+        display: flex;
+        height: 100%;
+        width: 100%;
+        background: rgba(0, 0, 0, 0.5);
+      }
+      .faye {
+        background-image: url("/Destinations/faye_canyon.JPG");
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 200px;
+        width: 270px;
+      }
+      .faye:hover > .overlay {
+        display: flex;
+        height: 100%;
+        width: 100%;
+        background: rgba(0, 0, 0, 0.5);
+      }
+      .ozarks {
+        background-image: url("/Destinations/ozarks.jpg");
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 200px;
+        width: 270px;
+      }
+      .ozarks:hover > .overlay {
+        display: flex;
+        height: 100%;
+        width: 100%;
+        background: rgba(0, 0, 0, 0.5);
+      }
+      .sedona {
+        background-image: url("/Destinations/cathedral_rock.jpg");
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 200px;
+        width: 270px;
+      }
+      .sedona:hover > .overlay {
+        display: flex;
+        height: 100%;
+        width: 100%;
+        background: rgba(0, 0, 0, 0.5);
+      }
+      .mt-ida {
+        background-image: url("/Destinations/mt_ida.jpg");
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 500px;
+        width: 500px;
+      }
+      .mt-ida:hover > .overlay {
+        display: flex;
+        height: 100%;
+        width: 100%;
+        background: rgba(0, 0, 0, 0.5);
+      }
+      .leconte {
+        background-image: url("/Destinations/tennessee.jpg");
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 300px;
+        width: 300px;
+      }
+      .leconte:hover > .overlay {
+        display: flex;
+        height: 100%;
+        width: 100%;
+        background: rgba(0, 0, 0, 0.5);
       }
     `}</style>
   </>
